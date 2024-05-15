4bad03b22f2067794e98d4145fe55bd4b3d9f46f
A7mooz <teaming04@gmail.com>
2024-05-15 12:28:59 +0300

    Made only for the package

--- src/Caclulator/ButtonListener.java
+++ src/Caclulator/ButtonListener.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class ButtonListener implements ActionListener {
+class ButtonListener implements ActionListener {
     private final Calculator calculator;
     ButtonListener(Calculator calculator) {
         this.calculator = calculator;
